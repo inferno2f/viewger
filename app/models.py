62b6432201d69cf6090a208244e659e134ecfb45
Vlad Nikitin <nikitinv91@gmail.com>
2022-04-10 14:48:03 +0300

    setting up db, orm and containers

--- app/models.py
+++ app/models.py
@@ -1,1 +1,23 @@
-# models to be added
+from views import db
+from sqlalchemy.dialects.postgresql import JSON
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    email = db.Column(db.String(128))
+    grade = db.Column(db.String(24))
+    is_admin = db.Column(db.Boolean, default=False, nullable=False)
+    result_all = db.Column(JSON)
+    result_no_stop_words = db.Column(JSON)
+
+    def __init__(self, username, email, result_all, result_no_stop_words):
+        self.username = username
+        self.email = email
+        self.result_all = result_all
+        self.result_no_stop_words = result_no_stop_words
+
+    def __repr__(self):
+        return f'{self.username} - id: {self.id}'
--- app/views.py
+++ app/views.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template
+import os
 
-"""
-Creates basic one-page Flask app
-"""
+from flask import Flask, render_template
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 viewgerapp = Flask(__name__)
+viewgerapp.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(viewgerapp)
+migrate = Migrate(viewgerapp, db)
 
 
 @viewgerapp.route("/")
 def index():
     return render_template("index.html")
+
+
+if __name__ == "__main__":
+    viewgerapp.run()

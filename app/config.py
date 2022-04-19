fe262c9802f6c09ff3b792010497db86548bd50a
Gennady Guniashev <genateepspb@gmail.com>
2022-04-19 12:17:46 +0300

    add black flake conf

--- app/config.py
+++ app/config.py
@@ -16,7 +16,7 @@ class Config:
     DB_HOST = os.environ.get("DB_HOST", "localhost")
     DB_PORT = os.environ.get("DB_PORT", 5432)
     DB_NAME = os.environ.get("DB_NAME", "viewgerdb")
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"  # noqa: E501
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 

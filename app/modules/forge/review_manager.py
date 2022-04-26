1722c034d4e3c68e4a1f016f8c933db38d4a2b14
Vlad Nikitin <nikitinv91@gmail.com>
2022-04-26 14:18:00 +0300

    Setting up logger for the project

--- app/modules/forge/review_manager.py
+++ app/modules/forge/review_manager.py
@@ -1,5 +1,9 @@
-from app.gitlab_client import gitlab_client
 from flask import current_app
+
+from app.gitlab_client import gitlab_client
+from app.logger_config import logger
+
+logger.name = __name__
 
 
 class ReviewManager:
@@ -7,8 +11,10 @@ class ReviewManager:
         """Assigns a reviewer to a merge request."""
         project = gitlab_client.projects.get(project_id)
         mr = project.mergerequests.get(merge_request_id)
+        logger.info(f'Assigning reviewer {reviewer_id} to merge request {mr.iid}')
         mr.manager.update(mr.get_id(), {'reviewer_ids': [reviewer_id]})
 
     def select_reviewer_for_mr(self):
         """Placeholder method for reviewer selection algorithm."""
+        logger.info('Selecting reviewer for merge request')
         return current_app.config['REVIEWER_ID']

64a13721b4342fa2282f21d682a548f3b1663e9e
Gennady Guniashev <genateepspb@gmail.com>
2022-05-04 09:01:26 +0300

    EPMLSTRVWG-19: upd commands.py endpoint.sh app.init

--- app/commands.py
+++ app/commands.py
@@ -1,13 +1,17 @@
 import click
+import logging
 
-from flask import current_app
+# from flask import Blueprint
 from flask.cli import with_appcontext
 
+# pull_gitlab_data = Blueprint('pull_gitlab_data', __name__)
 
-@click.command(name='pull-gitlab-data')
+logger = logging.getLogger(__name__)
+
+
+@click.command(name='pull_gitlab_data')
 @with_appcontext
 def pull_gitlab_data():
-    ...
-
-
-current_app.cli.add_command(pull_gitlab_data)
+    """Pulling project's data from Gitlab"""
+    logger.info("Pulling project's data from Gitlab")
+    print("Pulling project's data from Gitlab")

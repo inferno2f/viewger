43afdae0f7147eabff3ba224ef78800feefd6b5a
Gennady Guniashev <genateepspb@gmail.com>
2022-05-17 14:21:20 +0300

    EPMLSTRVWG-19: pull_project_data upd

--- app/services.py
+++ app/services.py
@@ -12,27 +12,39 @@ class ViewgerServices:
     def __init__(self, project_name):
         self.project_name = project_name
         project_name_space = f'epm-lstr/epm-lstr-vwg/{self.project_name}'
+        logger.info(f'Searching for {self.project_name} project on git.epam.com')
         self.project = gl.projects.get(project_name_space)
 
     def pull_project_data(self) -> Project:
         """
-        Gets project data from site
+        Fetch project info from gitlab.project & add them to the db
         """
-        logger.info(f'Searching for {self.project_name} project on git.epam.com')
         project_data = Project(
             forge_id=self.project.id,
             name=self.project.name,
             description=self.project.description,
             started_at=self.project.created_at,
         )
-        if not Project.query.filter_by(forge_id=self.project.id).first():
+        if not Project.query.first():
             db.session.add(project_data)
+            logger.info(f'Fetching {self.project_name} project info')
+            db.session.commit()
+        else:
+            Project.query.update(
+                {
+                    'forge_id': self.project.id,
+                    'name': self.project.name,
+                    'description': self.project.description,
+                    'started_at': self.project.created_at,
+                }
+            )
+            logger.info(f'Updating {self.project_name} project info')
             db.session.commit()
         return project_data
 
     def pull_project_members(self) -> None:
         """
-        Fetch members of a project from GitLab API, add them to the database
+        Fetch members of a project from gitlab.project & add them to the db
         """
         logger.info(f'Fetching members of {self.project.name} project')
         members = self.project.members.list()
